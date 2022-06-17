0ea2f301994d0f8550e0862f1ebfef9cf0eede00
Agamemnon <neil.a.rankin@googlemail.com>
2022-06-17 07:00:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# WipeCountdownAnnounce
+**WipeCountdownAnnounce** is an [Oxide](https://umod.org/) plugin that provides regular public chat announcements to players on a Rust server about the *time remaining until the next wipe*. Wipe information is supplied by the [WipeCountdown](https://codefling.com/plugins/wipe-countdown) plugin.
+
+Individually customizable messages are available for:
+- Standard days (e.g. *"X days, Y minutes until the next wipe"*).
+- Final day before the wipe (e.g. *"X hours, Y minutes until the next wipe"*).
+- Final hour before the wipe (e.g. *"X minutes until the next wipe"*).
+
+Additionally, a set list of timings can be used for the final hour before the wipe:
+- 60 minutes
+- 30 minutes
+- 15 minutes
+- 10 minutes
+- 5 minutes
+- 1 minute
+### Table of Contents  
+- [Requirements](#requirements)  
+- [Installation](#installation)  
+- [Permissions](#permissions)  
+- [Commands](#commands)  
+- [Configuration](#configuration)  
+- [Localization](#localization)  
+- [Credits](#credits)
+## Requirements
+| Depends On | Works With |
+| --- | --- |
+| [WipeCountdown](https://codefling.com/plugins/wipe-countdown) | |
+## Installation
+Download the plugin:
+```bash
+git clone https://github.com/rustireland/wipecountdownannounce.git
+```
+Copy it to the Oxide plugins directory:
+```bash
+cp wipecountdownannounce/WipeCountdownAnnounce.cs oxide/plugins
+```
+Oxide will compile and load the plugin automatically.
+## Permissions
+This plugin doesn't make use of the Oxide permissions system.
+## Commands
+This plugin doesn't provide any console or chat commands.
+## Configuration
+The settings and options can be configured in the `WipeCountdownAnnounce.json` file under the `oxide/config` directory. The use of an editor and validator is recommended to avoid formatting issues and syntax errors.
+
+When run for the first time, the plugin will create a default configuration file with all wipe announcements *enabled*.
+```json
+{
+  "Announcement Interval (in Minutes)": 20.0,
+  "Countdown Final Hour (60, 30, 15, 10, 5, 1 minutes)": true,
+  "Chat Icon (SteamID)": 0
+}
+```
+## Localization
+The default messages are in the `WipeCountdownAnnounce.json` file under the `oxide/lang/en` directory. To add support for another language, create a new language folder (e.g. **de** for German) if not already created, copy the default language file to the new folder and then customize the messages.
+```json
+{
+  "StandardMessage": "Next wipe will occur in <color=#ffa500>{days} days</color>, <color=#ffa500>{hours} hours</color>.",
+  "FinalDayMessage": "Next wipe will occur in <color=#ffa500>{hours} hours</color>, <color=#ffa500>{minutes} minutes</color>.",
+  "FinalHourMessage": "Next wipe will occur in <color=#ffa500>{minutes} minutes</color>."
+}
+```
+# Credits
+- [Agamemnon](https://github.com/agamemnon23) - Code, testing.
+- [Black_demon6](https://github.com/TheBlackdemon6) - Initial concept, testing, and French translations.
---
+++ WipeCountdownAnnounce.cs
@@ -0,0 +1,308 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using Oxide.Core;
+using Oxide.Core.Plugins;
+using Oxide.Core.Libraries.Covalence;
+
+namespace Oxide.Plugins
+{
+    [Info("WipeCountdownAnnounce", "Agamemnon", "1.0.0")]
+    [Description("Announces the time remaining until the next wipe to the server chat.")]
+    class WipeCountdownAnnounce : RustPlugin
+    {
+        [PluginReference] private Plugin WipeCountdown;
+
+        private ConfigData _configData;
+        private bool _wipeCountdownLoaded = false;
+        private Timer _announceTimer = null;
+        private Timer _announceSixtyTimer = null;
+        private Timer _announceThirtyTimer = null;
+        private Timer _announceFifteenTimer = null;
+        private Timer _announceTenTimer = null;
+        private Timer _announceFiveTimer = null;
+        private Timer _announceOneTimer = null;
+
+        #region Oxide Hooks
+        private void OnServerInitialized()
+        {
+            if (!LoadConfigVariables())
+            {
+                PrintError("ERROR: The config file is corrupt. Either fix or delete it and restart the plugin.");
+                PrintError("ERROR: Unloading plugin.");
+                Interface.Oxide.UnloadPlugin(this.Title);
+                return;
+            }
+
+            if (WipeCountdown == null)
+            {
+                PrintWarning("The WipeCountdown plugin isn't loaded. Wipe announcements are suspended.");
+                _wipeCountdownLoaded = false;
+            }
+            else
+            {
+                StartTimers();
+                _wipeCountdownLoaded = true;
+            }
+        }
+
+        private void OnPluginLoaded(Plugin name)
+        {
+            if (name.ToString() == "Oxide.Plugins.WipeCountdown")
+            {
+                PrintWarning("The WipeCountdown plugin has been loaded. Wipe announcements will resume.");
+                StartTimers();
+                _wipeCountdownLoaded = true;
+            }
+        }
+
+        private void OnPluginUnloaded(Plugin name)
+        {
+            if (name.ToString() == "Oxide.Plugins.WipeCountdown")
+            {
+                PrintWarning("The WipeCountdown plugin has been unloaded. Wipe announcements are suspended.");
+                StopTimers();
+                _wipeCountdownLoaded = false;
+            }
+        }
+        #endregion
+
+        #region Helper Functions
+        private void StartTimers()
+        {
+            if (_announceTimer == null)
+            {
+                _announceTimer = timer.Every(_configData.AnnounceInterval * 60, BroadcastAnnouncement);
+            }
+
+            if (_configData.CountdownEnabled)
+            {
+                long timeleft = GetCountdownSeconds();
+                if (timeleft > 3600)
+                {   
+                    if (_announceSixtyTimer == null)
+                    {
+                        timeleft = GetCountdownSeconds();
+                        _announceSixtyTimer = timer.Once(timeleft - 3600, () => BroadcastCountdown(60));
+                    }
+                }
+                if (timeleft > 1800)
+                {   
+                    if (_announceThirtyTimer == null)
+                    {
+                        timeleft = GetCountdownSeconds();
+                        _announceThirtyTimer = timer.Once(timeleft - 1800, () => BroadcastCountdown(30));
+                    }
+                }
+                if (timeleft > 900)
+                {   
+                    if (_announceFifteenTimer == null)
+                    {
+                        timeleft = GetCountdownSeconds();
+                        _announceFifteenTimer = timer.Once(timeleft - 900, () => BroadcastCountdown(15));
+                    }
+                }
+                if (timeleft > 600)
+                {   
+                    if (_announceTenTimer == null)
+                    {
+                        timeleft = GetCountdownSeconds();
+                        _announceTenTimer = timer.Once(timeleft - 600, () => BroadcastCountdown(10));
+                    }
+                }
+                if (timeleft > 300)
+                {   
+                    if (_announceFiveTimer == null)
+                    {
+                        timeleft = GetCountdownSeconds();
+                        _announceFiveTimer = timer.Once(timeleft - 300, () => BroadcastCountdown(5));
+                    }
+                }
+                if (timeleft > 60)
+                {   
+                    if (_announceOneTimer == null)
+                    {
+                        timeleft = GetCountdownSeconds();
+                        _announceOneTimer = timer.Once(timeleft - 60, () => BroadcastCountdown(1));
+                    }
+                }
+            }
+        }
+
+        private void StopTimers()
+        {
+            if (_announceTimer != null)
+            {
+                _announceTimer.Destroy();
+                _announceTimer = null;
+            }
+            if (_announceSixtyTimer != null)
+            {
+                _announceSixtyTimer.Destroy();
+                _announceSixtyTimer = null;
+            }
+            if (_announceThirtyTimer != null)
+            {
+                _announceThirtyTimer.Destroy();
+                _announceThirtyTimer = null;
+            }
+            if (_announceFifteenTimer != null)
+            {
+                _announceFifteenTimer.Destroy();
+                _announceFifteenTimer = null;
+            }
+            if (_announceTenTimer != null)
+            {
+                _announceTenTimer.Destroy();
+                _announceTenTimer = null;
+            }
+            if (_announceFiveTimer != null)
+            {
+                _announceFiveTimer.Destroy();
+                _announceFiveTimer = null;
+            }
+            if (_announceOneTimer != null)
+            {
+                _announceOneTimer.Destroy();
+                _announceOneTimer = null;
+            }
+        }
+
+        private void BroadcastAnnouncement()
+        {
+            if (_wipeCountdownLoaded)
+            {
+                long timeleft = GetCountdownSeconds();
+                TimeSpan timeleftFormatted = TimeSpan.FromSeconds(timeleft);
+                int days = timeleftFormatted.Days;
+                int hours = timeleftFormatted.Hours;
+                int minutes = timeleftFormatted.Minutes;
+                int seconds = timeleftFormatted.Seconds;
+
+                if (days >= 0 && hours >= 0 && minutes >= 0 && seconds >= 0)
+                {
+                    if (timeleft > 86400)
+                    {   
+                        Server.Broadcast(Lang("StandardMessage",
+                            new KeyValuePair<string, string>("days", days.ToString()),
+                            new KeyValuePair<string, string>("hours", hours.ToString()),
+                            new KeyValuePair<string, string>("minutes", minutes.ToString()),
+                            new KeyValuePair<string, string>("seconds", seconds.ToString())), _configData.ChatIcon);
+                    }
+                    else if (timeleft <= 3660 && _configData.CountdownEnabled)
+                    {
+                        // Do nothing
+                    }
+                    else if (timeleft < 3600)
+                    {
+                        Server.Broadcast(Lang("FinalHourMessage",
+                            new KeyValuePair<string, string>("minutes", minutes.ToString())), _configData.ChatIcon);
+                    }
+                    else
+                    {
+                        Server.Broadcast(Lang("FinalDayMessage",
+                            new KeyValuePair<string, string>("days", days.ToString()),
+                            new KeyValuePair<string, string>("hours", hours.ToString()),
+                            new KeyValuePair<string, string>("minutes", minutes.ToString()),
+                            new KeyValuePair<string, string>("seconds", seconds.ToString())), _configData.ChatIcon);
+                    }
+                }
+            }
+        }
+
+        private void BroadcastCountdown(int minutes)
+        {
+            if (_wipeCountdownLoaded)
+            {
+                Server.Broadcast(Lang("FinalHourMessage",
+                    new KeyValuePair<string, string>("minutes", minutes.ToString())), _configData.ChatIcon);
+            }
+        }
+        #endregion
+
+        #region Plugin API Calls
+        private long GetCountdownSeconds()
+        {
+            long seconds = (long)WipeCountdown?.Call("GetCountdownSeconds_API");
+            return seconds;
+        }
+
+        private string GetCountdownFormatted()
+        {
+            string formatted = (string)WipeCountdown?.Call("GetCountdownFormated_API");
+            return formatted;
+        }
+        #endregion
+
+        #region Configuration
+        private class ConfigData
+        {
+            [JsonProperty(PropertyName = "Announcement Interval (in Minutes)")]
+            public float AnnounceInterval = 20.0f;
+
+            [JsonProperty(PropertyName = "Countdown Final Hour (60, 30, 15, 10, 5, 1 minutes)")]
+            public bool CountdownEnabled = true;
+
+            [JsonProperty(PropertyName = "Chat Icon (SteamID)")]
+            public ulong ChatIcon = 76561199034048902;
+        }
+
+        private bool LoadConfigVariables()
+        {
+            try
+            {
+                _configData = Config.ReadObject<ConfigData>();
+            }
+            catch
+            {
+                return false;
+            }
+
+            SaveConfig(_configData);
+            return true;
+        }
+
+        protected override void LoadDefaultConfig()
+        {
+            PrintWarning("Creating new config file.");
+            _configData = new ConfigData();
+            SaveConfig(_configData);
+        }
+
+        private void SaveConfig(ConfigData config)
+        {
+            Config.WriteObject(config, true);
+        }
+        #endregion
+
+        #region Language
+        protected override void LoadDefaultMessages()
+        {
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                ["StandardMessage"] = "Next wipe will occur in <color=#ffa500>{days} days</color>, <color=#ffa500>{hours} hours</color>.",
+                ["FinalDayMessage"] = "Next wipe will occur in <color=#ffa500>{hours} hours</color>, <color=#ffa500>{minutes} minutes</color>.",
+                ["FinalHourMessage"] = "Next wipe will occur in <color=#ffa500>{minutes} minutes</color>."
+
+            }, this);
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                ["StandardMessage"] = "Le prochain wipe aura lieu dans <color=#ffa500>{days} jours</color>, <color=#ffa500>{hours} heures</color>.",
+                ["FinalDayMessage"] = "Le prochain wipe aura lieu dans <color=#ffa500>{hours} heures</color>, <color=#ffa500>{minutes} minutes</color>.",
+                ["FinalHourMessage"] = "Le prochain wipe aura lieu dans <color=#ffa500>{minutes} minutes</color>."
+            }, this, "fr");
+        }
+
+        private string Lang(string key) => string.Format(lang.GetMessage(key, this));
+        private string Lang(string key, params KeyValuePair<string, string>[] replacements)
+        {
+            var message = lang.GetMessage(key, this);
+
+            foreach (var replacement in replacements)
+                message = message.Replace($"{{{replacement.Key}}}", replacement.Value);
+
+            return message;
+        }
+        #endregion
+    }
+}
